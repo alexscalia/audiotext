5ae4d15de7b24b38dc6b636196d324f394c1088c
Alex Scalia <alexscalia75@gmail.com>
2026-05-05 22:42:05 +0700

    feat(migrations): add initial migration for voice and chat features
    - Created new migration file `0000_parched_mister_sinister.sql` to set up database schema for voice and chat functionalities.
    - Introduced various ENUM types for voice termination and carrier statuses.
    - Defined tables for accounts, voice numbers, terminations, carriers, chat contacts, countries, permissions, roles, sessions, users, and voice CDRs.
    - Established foreign key constraints and indexes to ensure data integrity and optimize query performance.
    - Updated journal to reflect the new migration version and tag.

--- packages/db/migrations/0001_great_stone_men.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "carriers" ADD COLUMN "rates_phone" text;--> statement-breakpoint
-ALTER TABLE "carriers" ADD COLUMN "billing_phone" text;--> statement-breakpoint
-ALTER TABLE "carriers" ADD COLUMN "noc_phone" text;--> statement-breakpoint
-ALTER TABLE "carriers" ADD COLUMN "sales_phone" text;
